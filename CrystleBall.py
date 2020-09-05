ccd9ad16c37f8bd70aa8c381c670af063d29aa45
AnweshaDugar <70806940+AnweshaDugar@users.noreply.github.com>
2020-09-05 18:37:08 +0530

    Add files via upload

---
+++ CrystleBall.py
@@ -0,0 +1,41 @@
+import random
+
+response = random.randint(0, 6)
+
+question = input("Welcome to Crystal Ball! Ask me any question:")
+
+if response == 0:
+    print("Yes")
+else:
+    if response == 1:
+        print ("No")
+    else:
+        if response == 2:
+            print ("Maybe")
+
+        else:
+            if response == 3:
+                print ("Go with your gut feeling on this one. I cant help!")
+
+           
+            else:
+                  if response == 4:
+                      print ("Totally!")
+
+                      
+                  else:
+
+                       if response == 5:
+                           print ("Dont count on it!")
+
+
+                      
+                       else:
+
+                            if response == 6:
+                                print ("I Think I cant help you in this")
+                                
+                                      
+
+
+                            
